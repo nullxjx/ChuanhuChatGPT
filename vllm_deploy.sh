d69ecb65ec979722295178bce6501d0183d1eac7
jasinxie <jasinxie@tencent.com>
2024-03-13 15:01:46 +0800

    修改vllm部署命令中的模型位置

--- vllm_deploy.sh
+++ vllm_deploy.sh
@@ -5,10 +5,10 @@
 # conda activate vllm
 
 # 部署Qwen模型，模型名字 Qwen1.5-4B-Chat
-python -m vllm.entrypoints.openai.api_server --model Qwen/Qwen1.5-4B-Chat --dtype=float16 --max-model-len 8192 --served-model-name Qwen1.5-4B-Chat --port 8000
+python -m vllm.entrypoints.openai.api_server --model /root/models/Qwen1.5-4B-Chat --dtype=float16 --max-model-len 8192 --served-model-name Qwen1.5-4B-Chat --port 8000
 
 # 部署Yi模型，模型名字 Yi-6B-Chat
-#python -m vllm.entrypoints.openai.api_server --model 01-ai/Yi-6B-Chat --dtype=float16 --max-model-len 4096 --served-model-name Yi-6B-Chat --port 8000
+#python -m vllm.entrypoints.openai.api_server --model /root/models/Yi-6B-Chat --dtype=float16 --max-model-len 4096 --served-model-name Yi-6B-Chat --port 8000
 
 # 部署chatglm，模型名字 chatglm3-6b-32k
-#python -m vllm.entrypoints.openai.api_server --model THUDM/chatglm3-6b-32k --dtype=float16 --max-model-len 8192 --served-model-name chatglm3-6b-32k --port 8000 --trust-remote-code --gpu-memory-utilization 0.95
+#python -m vllm.entrypoints.openai.api_server --model /root/models/chatglm3-6b-32k --dtype=float16 --max-model-len 8192 --served-model-name chatglm3-6b-32k --port 8000 --trust-remote-code --gpu-memory-utilization 0.95
